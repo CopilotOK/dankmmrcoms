c0ca7b2b6a201b8b57c50b61cf4b49d6edd31f85
a <97122477+CopilotOK@users.noreply.github.com>
2024-03-13 20:49:18 +0530

    Add files via upload

---
+++ arravandsk.py
@@ -0,0 +1,89 @@
+from pyautogui import moveTo, click, write, alert, press
+from time import sleep
+
+alert(title="ALERT",text='''move this window over discord window
+go to shashwat gautam general #dank-memer
+then press OK''')
+def textbox():
+    moveTo(x=414,y=670)
+def hunt():
+    write("/hunt")
+    sleep(1)
+    click(x=460,y=492)
+    sleep(1)
+    press("enter")
+    
+ 
+def beg():
+    write("/beg")
+    sleep(1)
+    click(x=577,y=602)
+    sleep(1)
+    press("enter")
+   
+    
+def dig():
+    write("/dig")
+    sleep(1)
+    click(x=577,y=602)
+    sleep(1)
+    press("enter")
+    
+def search():
+    write("/search")
+    sleep(1)
+    click(x=448,y=215)
+    sleep(1)
+    press("enter")
+    
+    sleep(2)
+    click(x=422,y=602)
+def postmemes():
+    write("/postmemes")
+    sleep(1)
+    click(x=577,y=602)
+    sleep(1)
+    press("enter")   
+    sleep(1)
+    click(x=592,y=513)
+    sleep(1)
+    click(x=628,y=343)
+    sleep(1)
+    click(x=534,y=556)
+    sleep(1)
+    click(x=576,y=475)
+    sleep(1)
+    click(x=416,y=607)
+def crime():
+    write("/crime")
+    sleep(1)
+    click(x=577,y=602)
+    sleep(1)
+    press("enter")
+    sleep(2)
+    click(x=422,y=602)
+
+while True:
+    textbox()
+    sleep(1)
+    hunt()
+    sleep(2)
+    beg()
+    sleep(2)
+    dig()
+    sleep(2)
+    search()
+    sleep(2)
+    postmemes()
+    sleep(2)
+    crime()
+    textbox()
+    sleep(0.5)
+    write("waiting...")
+    sleep(5)
+    write("not anymore")
+    press("enter")
+
+
+
+    
